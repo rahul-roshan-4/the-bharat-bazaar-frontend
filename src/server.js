a05cb8154ac2e524ab2eacf36d173ae9adf5c0a4
rahul-roshan-4 <101462374+rahul-roshan-4@users.noreply.github.com>
2024-06-18 18:27:33 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
+export const server = "https://the-bharat-bazaar-backend.onrender.com/api/v2";
 
 
